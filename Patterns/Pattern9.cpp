2b7b7c5c193533917fac051d4dd45545895379a7
SakshiRai01 <0111sakshirai@gmail.com>
2022-03-02 23:06:30 +0530

    Pattern 9 to print *

---
+++ Patterns/Pattern9.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int n;
+    cout << "Enter n to print the pattern : ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j || i + j == n - 1)
+            {
+                cout << "*\t";
+            }
+            else
+            {
+                cout << "\t";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
